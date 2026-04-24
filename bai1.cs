a0b3be7a0762aa5fabe140c29df34ac55d757344
thamthach31251022786-ux <thamthach.31251022786@st.ueh.edu.vn>
2026-04-24 21:33:58 +0700

    Create bai1.cs

---
+++ bai1.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Nhap so a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Nhap so b: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Tong = " + (a + b));
+    }
+}
