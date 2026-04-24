05a8f7f6d570fec155bbbd9be58eaf7bb91a58dd
thamthach31251022786-ux <thamthach.31251022786@st.ueh.edu.vn>
2026-04-24 21:49:06 +0700

    Create bai6.cs

---
+++ bai6.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine($"Size of char: {sizeof(char)} bytes");
+        Console.WriteLine($"Size of int: {sizeof(int)} bytes");
+        Console.WriteLine($"Size of float: {sizeof(float)} bytes");
+        Console.WriteLine($"Size of double: {sizeof(double)} bytes");
+    }
+}
